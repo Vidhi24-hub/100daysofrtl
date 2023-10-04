224df307c72336448c7aae0212daebb656b8783f
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-10-04 19:45:16 +0530

    Update demux.v

--- Day-15-DEMUX/demux.v
+++ Day-15-DEMUX/demux.v
@@ -2,7 +2,7 @@
 
 module demux(in,sel,out);
 parameter n=3'd4; //number of select lines
-parameter m=2**n; // to figure out width of output
+parameter m=2**n; //number of output lines
 input in; 
 input [n-1:0] sel;
 output reg [m-1:0] out;
