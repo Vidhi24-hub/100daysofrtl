9426bef9d45e8e684b3659bf9403ac3b1a8d47cf
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-10-09 19:16:31 +0530

    Update sr_latch.v

--- Day-20-SR-Latch/sr_latch.v
+++ Day-20-SR-Latch/sr_latch.v
@@ -17,7 +17,7 @@ begin
 		 2'b01 : begin q<=0; q_bar<=1; end
 		 2'b10 : begin q<=1; q_bar<=0; end
 		 2'b11 : begin q<=0; q_bar<=0; end
-		 default : begin q<=1'b0; q_bar<=1'b0; end
+		 default : begin q<=1'bx; q_bar<=1'bx; end
 	    endcase
    end
 end
