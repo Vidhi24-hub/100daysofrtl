d1800c529e61854ba20b1d58bd52a27dac3d6cec
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-10-09 19:16:02 +0530

    Update sr_latch.v

--- Day-20-SR-Latch/sr_latch.v
+++ Day-20-SR-Latch/sr_latch.v
@@ -12,12 +12,12 @@ begin
    end
    else if(en) 
    begin
-	    case({j,k})       
+	   case({s,r})       
 		 2'b00 : begin q<=q; q_bar<=q_bar; end
 		 2'b01 : begin q<=0; q_bar<=1; end
 		 2'b10 : begin q<=1; q_bar<=0; end
 		 2'b11 : begin q<=0; q_bar<=0; end
-		 default : begin q<=1'bx; q_bar<=1'bx; end
+		 default : begin q<=1'b0; q_bar<=1'b0; end
 	    endcase
    end
 end
