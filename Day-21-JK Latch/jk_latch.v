c7bd092e41177456dbec07d8419a4f6d6292a422
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-10-10 19:42:16 +0530

    Update jk_latch.v

--- Day-21-JK Latch/jk_latch.v
+++ Day-21-JK Latch/jk_latch.v
@@ -17,7 +17,7 @@ begin
 		 2'b00 :   begin q<=q;    q_bar<=q_bar; end
 		 2'b01 :   begin q<=0;    q_bar<=1; end
 		 2'b10 :   begin q<=1;    q_bar<=0; end
-		 2'b11 :   begin q<=1;    q_bar<=1; end
+		 2'b11 :   begin q<=~q;    q_bar<=~q_bar; end
 		 default : begin q<=1'bx; q_bar<=1'bx; end
 		 endcase
 		 
