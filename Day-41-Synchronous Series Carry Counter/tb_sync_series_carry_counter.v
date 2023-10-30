ecc73486961b3a203a84cf6f13bbd5c6bf5b00b3
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-10-30 21:17:53 +0530

    Update tb_sync_series_carry_counter.v

--- Day-41-Synchronous Series Carry Counter/tb_sync_series_carry_counter.v
+++ Day-41-Synchronous Series Carry Counter/tb_sync_series_carry_counter.v
@@ -1,15 +1,15 @@
 module tb_sync_series_carry_counter();
-  reg clk,rst;
+  reg clk;
   wire [3:0] out;
   
-  sync_series_carry_counter dut(clk,rst,out);
+  sync_series_carry_counter dut(clk,out);
   
   always #3 clk = ~clk;
   
   initial begin
     
-    #0; clk=0; rst=1;
-    #4; rst=0;
+    #0; clk=0; 
     #100; $finish;
+    
   end
 endmodule
