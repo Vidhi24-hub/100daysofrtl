69e8a4a9b5e026b721f72d24f3709536906cd01d
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-11-09 01:14:52 +0530

    Update tb_booth.v

--- Day-50-Booth Multiplier/tb_booth.v
+++ Day-50-Booth Multiplier/tb_booth.v
@@ -1,16 +1,16 @@
-module tb_booth;
-  reg [3:0] m,q_in;
+module tb_booth_mul;
+  reg [3:0] m,q;
   wire [7:0] mul;
   
-  booth_mul dut(m,q_in,mul);
+  booth_mul dut(m,q,mul);
   
   initial begin
     
-
-    
-    #0; m=0; q_in=0;
-    #3; m=4'h7; q_in=4'h3;
-    #3; m=4'h6; q_in=4'h4;
+    #0; m=0; q=0;
+    #3; m=4'h7; q=4'h3;
+    #3; m=4'h6; q=4'h4;
+    #3; m=4'hc; q=4'h9;
+    #3; m=4'hb; q=4'hb;
     #3; $finish;
    
   end
