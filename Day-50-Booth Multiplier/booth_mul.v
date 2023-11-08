3ccbbe420ec6ba4d787ada8546c91e72aa7ad623
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-11-09 01:13:50 +0530

    Update booth_mul.v

--- Day-50-Booth Multiplier/booth_mul.v
+++ Day-50-Booth Multiplier/booth_mul.v
@@ -1,10 +1,11 @@
-module booth_mul(a,b,mul);
-  input [3:0] a,b;
+module booth_mul(m,q,mul);
+  input [3:0] m,q; //m is multiplicand and q is multiplier
   output [7:0] mul;
   
-  reg [3:0] Acc, q; //Acc is Accumulator and q is multiplier 
+  reg [3:0] Acc; //Acc is Accumulator  
   reg p; //p is q(-1)
-  reg [8:0] con;
+  reg [8:0] con; 
+  
   integer i;
   initial Acc=0;
   initial p=0;
@@ -12,28 +13,28 @@ module booth_mul(a,b,mul);
   always@(*)
     begin
       
-      q=b;
-      
-      con={Acc,b,p};
+      con = {Acc,q,p}; //con is concatenation of Acc, q and p
 		
       for(i=0;i<4;i=i+1)
         begin
           
           if({con[1],con[0]} == 2'b00 | {con[1],con[0]} == 2'b11)
-            begin con={con[1],con[8:1]}; end
+            begin con = {con[8],con[8:1]}; end
           
           else if({con[1],con[0]} == 2'b01)
-            begin con[8:5]=con[8:5]+a; con={con[1],con[8:1]}; end
+            begin con[8:5] = con[8:5] + m; con = {con[8],con[8:1]}; end
           
           else if({con[1],con[0]} == 2'b10)
-            begin con[8:5]=con[8:5]-a; con={con[1],con[8:1]}; end
+            begin con[8:5] = con[8:5] - m; con = {con[8],con[8:1]}; end
+          
+          else
+            begin con = {Acc,q,p}; end
 
-         
         end
      
     end
 	 
-  assign  mul={con[8:1]};
+  assign  mul = {con[8:1]};
   
 endmodule
  
