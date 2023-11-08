f7c2f4eb695d630574fef74b4028c000ac53d440
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-11-09 01:19:58 +0530

    Update tb_vedic_multiplier.v

--- Day-49-Vedic Multiplier/tb_vedic_multiplier.v
+++ Day-49-Vedic Multiplier/tb_vedic_multiplier.v
@@ -5,9 +5,6 @@ module tb_vedic_mul_8_bit;
   vedic_mul_8_bit dut(a,b,mul);
   
   initial begin
-    
-    $dumpfile("dump.vcd");
-    $dumpvars();
    
     #0; a=0; b=0;
     #3; a=8'h21; b=8'h45;
