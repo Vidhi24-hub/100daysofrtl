fadca978b54cc5e25c3f2e12fe1469fb2aef0880
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-11-10 18:54:15 +0530

    Update dual_port_ram.v

--- Day-52-Dual Port RAM/dual_port_ram.v
+++ Day-52-Dual Port RAM/dual_port_ram.v
@@ -1,7 +1,7 @@
-module dual_port_ram(data_a,data_b,addr_a,addr_b,we_a,we_b,clk,q_a,q_b);
+module dual_port_ram(data_a,data_b,addr_a,addr_b,we_a,we_b,clk,rst,q_a,q_b);
   input [7:0] data_a,data_b; //data input
   input [5:0] addr_a,addr_b; //address input
-  input we_a,we_b,clk;       //we is write enable
+  input we_a,we_b,clk,rst;       //we is write enable
   output reg [7:0] q_a,q_b;      
   
   reg [7:0] ram [63:0]; //64*8 bit ram 
@@ -9,7 +9,9 @@ module dual_port_ram(data_a,data_b,addr_a,addr_b,we_a,we_b,clk,q_a,q_b);
   always@(posedge clk)
     begin
       
-      if(we_a)
+      if(rst)
+        begin ram[addr_a] <= 0; q_a = ram[addr_a]; end
+      else if(we_a)
         ram[addr_a] <= data_a;
       else
         q_a <= ram[addr_a];
@@ -19,7 +21,9 @@ module dual_port_ram(data_a,data_b,addr_a,addr_b,we_a,we_b,clk,q_a,q_b);
   always@(posedge clk)
     begin
       
-      if(we_b)
+      if(rst)
+        begin ram[addr_b] <= 0; q_b = ram[addr_b]; end
+      else if(we_b)
         ram[addr_b] <= data_b;
       else
         q_b <= ram[addr_b];
