e8d3d4da94731f621c699f87f1c4642697a9f16d
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-11-27 22:17:52 +0530

    Add files via upload

---
+++ Day-69-Mealy Overlapping Sequence Detector/tb_mealy_overlapping.v
@@ -0,0 +1,27 @@
+module tb_mealy_overlapping();
+  reg in,clk,rst;
+  wire out;
+  
+  mealy_overlapping dut(in,clk,rst,out); //1101 sequence detector
+  
+  always #2 clk=~clk;
+  
+  initial begin
+    
+    #0; clk=0; rst=1;
+    #3; rst=0;
+    @(negedge clk) in=0;
+    @(negedge clk) in=1;
+    @(negedge clk) in=1;
+    @(negedge clk) in=0;
+    @(negedge clk) in=1;
+    @(negedge clk) in=1;
+    @(negedge clk) in=0;
+    @(negedge clk) in=1;
+    @(negedge clk) in=1;
+    @(negedge clk) in=0;
+    @(negedge clk) in=1;
+    
+    #100; $finish;
+  end
+endmodule
