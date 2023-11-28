d4271ca55765b78f01225c531b607df3b6b997b1
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-11-28 19:59:13 +0530

    Add files via upload

---
+++ Day-70-Moore Non-overlapping Sequence Detector/moore_non_overlapping.v
@@ -0,0 +1,63 @@
+module moore_non_overlapping(in,clk,rst,out); //1011 sequence
+  input in,clk,rst;
+  output reg out;
+  
+  //reg comb_out, seq_out;
+  
+  reg [2:0] presentstate, nextstate;
+  parameter S0=0, S1=1, S10=2, S101=3, S1011=4;
+  
+  always@(posedge clk)
+    begin
+      if(rst)
+        begin
+          presentstate <= S0;
+        end
+      else 
+        presentstate <= nextstate;
+    end
+  
+  always@(presentstate or in)
+    begin
+      
+      case(presentstate)
+        
+        S0 :
+          begin
+            out = 0;
+            nextstate = in ? S1 : S0;
+          end
+        
+        S1 :
+          begin
+            out = 0;
+            nextstate = in ? S1 : S10;
+          end
+        
+        S10 :
+          begin
+            out = 0;
+            nextstate = in ? S101 : S0;
+          end
+        
+        S101 :
+          begin
+            out = 0;
+            nextstate = in ? S1011 : S10;
+          end
+        
+        S1011 :
+          begin
+            out = 1;
+            nextstate = in ? S1 : S0;
+          end
+        default : 
+          begin
+            out = 1'bz;
+            nextstate = S0;
+          end
+      endcase
+    end
+endmodule
+      
+  
---
+++ Day-70-Moore Non-overlapping Sequence Detector/tb_moore_non_overlapping.v
@@ -0,0 +1,24 @@
+module tb_moore_non_overlapping();
+  reg in,clk,rst;
+  wire out;
+  
+  moore_non_overlapping dut(in,clk,rst,out); //1011 sequence detector
+  
+  always #2 clk=~clk;
+  
+  initial begin
+    
+    #0; clk=0; rst=1;
+    #3; rst=0;
+    @(negedge clk) in=1;
+    @(negedge clk) in=0;
+    @(negedge clk) in=1;
+    @(negedge clk) in=1;
+    #3; rst=1;
+    @(negedge clk) in=0;
+    @(negedge clk) in=1;
+    @(negedge clk) in=1;
+    
+    #100; $finish;
+  end
+endmodule
