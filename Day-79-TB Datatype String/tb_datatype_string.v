3ab794b9c659c7148e08d0809e0f714565138fee
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-12-07 18:05:36 +0530

    Add files via upload

---
+++ Day-79-TB Datatype String/tb_datatype_string.v
@@ -0,0 +1,17 @@
+module tb_datatype_string;
+  string s1 = "VLSI Enthuasianst";
+  string s2 = {"Hello", " ", s1};
+  bit [31:0] num = 78;
+  string s3 = num;
+  
+  initial begin
+    
+    $display("String : %s",s2);
+    $display("Today is my%d th day of 100daysofrtl",s3);
+    
+  end
+endmodule
+             
+             
+             
+  
