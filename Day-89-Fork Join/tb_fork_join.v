e74cac3e3934ad49ac68149ba5c03cd6acf42542
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2023-12-17 21:56:16 +0530

    Add files via upload

---
+++ Day-89-Fork Join/tb_fork_join.v
@@ -0,0 +1,37 @@
+module tb_fork_join;
+  
+  initial begin
+    
+    statement(1,"Beginning of Fork-Join");
+    
+    fork
+
+      //Thread 1
+      fork
+        statement(20,"Thread 1_1");
+        statement(30,"Thread 1_2");
+      join
+      
+    //Thread 2
+    begin
+      statement(2,"Thread 2_1");
+      statement(3,"Thread 2_2");
+    end
+    
+    //Thread 3
+      statement(25,"Thread 3");
+      
+    join
+    
+    statement($time,"Ending of Fork-Join");
+    
+  end
+ 
+  //automatic task
+  task automatic statement(int t, string t_name);
+    #t $display("%0t ns %s", $time,t_name);
+  endtask      
+      
+endmodule
+        
+        
