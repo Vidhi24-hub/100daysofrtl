0970c5fbc4e41c8c74fb6af3f19670d4f1310b2e
Vidhi Patel <83576362+Vidhi24-hub@users.noreply.github.com>
2024-03-28 00:56:14 +0530

    Update tb_barrel_shifter.v

--- Day-60-Barrel Shifter/tb_barrel_shifter.v
+++ Day-60-Barrel Shifter/tb_barrel_shifter.v
@@ -1,16 +1,16 @@
 module tb_barrel_shifter();
   reg [7:0] in;
   reg [2:0] shift;
+  reg mode;
   wire [7:0] out;
   
-  barrel_shifter dut(in,shift,out);
+  barrel_shifter dut(in,shift,mode,out);
   
   initial begin
-
-    #0; in=0; shift=0;
+    
+    #0; in=0; shift=0; mode=0;
     #3; in=8'hb; shift=3'h2;
-    #3; in=8'h5; shift=3'h5;
+    #3; in=8'ha; shift=3'h5; mode=1;
     #3; $finish;
-
   end
 endmodule
